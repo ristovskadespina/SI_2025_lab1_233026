2b7eea682eb59f4135f1b0cd114e40ac653968ac
ristovskadespina <despina.ristovska@students.finki.ukim.mk>
2025-06-06 14:56:55 +0200

    Implement sortTasksByName method

--- SI2025Lab1Main.java
+++ SI2025Lab1Main.java
@@ -75,6 +75,7 @@ class TaskManager {
 
     // 3. List tasks sorted by name
     public void sortTasksByName() {
+        tasks.sort(Comparator.comparing(Task::getName));
         // TODO: Implement sorting logic
     }
 
